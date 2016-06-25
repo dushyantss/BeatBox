1bc3f21bd80947e5bc8ae7524b9dd0cc334243fc
Dushyant Singh Shekhawat <dushyant225@gmail.com>
2016-06-26 00:06:25 +0530

    Used Soundpool to make sounds working

--- app/src/main/java/dushyant/android/beatbox/models/BeatBox.java
+++ app/src/main/java/dushyant/android/beatbox/models/BeatBox.java
@@ -1,7 +1,10 @@
 package dushyant.android.beatbox.models;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -16,12 +19,17 @@ public class BeatBox {
     private static final String TAG = "BeatBox";
 
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        //This old constructor is deprecated, but we need it
+        //for compatibility
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
 
@@ -35,14 +43,36 @@ public class BeatBox {
             return;
         }
 
-        for (String filename :
-                soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            mSounds.add(new Sound(assetPath));
+        for (String filename : soundNames) {
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+                Log.e(TAG, "Could not load sound " + filename, e);
+            }
         }
     }
 
     public List<Sound> getSounds() {
         return mSounds;
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
+    }
+
+    public void play(Sound sound) {
+        if (sound.getSoundId() == null) {
+            return;
+        }
+        mSoundPool.play(sound.getSoundId(), 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void release() {
+        mSoundPool.release();
     }
 }
--- app/src/main/java/dushyant/android/beatbox/models/Sound.java
+++ app/src/main/java/dushyant/android/beatbox/models/Sound.java
@@ -7,12 +7,21 @@ package dushyant.android.beatbox.models;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
         String[] components = assetPath.split("/");
         String name = components[components.length - 1];
         mName = name.replace(".wav", "");
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 
     public String getAssetPath() {
